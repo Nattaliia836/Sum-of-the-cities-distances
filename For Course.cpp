e1c10caa46b132ec42802886a0db95fb11edf986
Nattaliia836 <72167706+Nattaliia836@users.noreply.github.com>
2021-10-05 16:13:44 +0300

    Update For Course.cpp

--- For Course.cpp
+++ For Course.cpp
@@ -4,48 +4,41 @@
 
 using namespace std;
 
-
-void sum(int s                     // Значення суму одних із к елементів
-    , const vector<int>& v       // вектор з усіма данними
+void sum(int s                  // Значення суму одних із к елементів
+    , const vector<int>& v      // вектор з усіма данними
     , int i                     // номер елементу, який треба додати
     , int count                 // чисто елементів які ще треба додати
-    , const int& k
-    , vector<int>& result
+    , const int& k              // кількість ітерацій
+    , vector<int>& result       // вектор з результатом
     , int number                // інтекс попереднього доданого в суму числа
-    //, const int& k              // кількість ітерацій
 ) {
     if (count > 1) {
         count--;
         i++;
-        for (int j = number + 1; j <  v.size() - (k - (i - 1)); j++) { // j = 2-2 =0; j<2-1=1 (0<1); ++i 
+        for (int j = number + 1; j <  v.size() - (k - (i - 1)); j++) { 
             sum((s + v[j]), v, i, count, k, result, j);
         }
     }
     else if (count <= 1) {
-        for (int j = number + 1; j < v.size(); j++) { // j=2-1, j< 2-2 , 
+        for (int j = number + 1; j < v.size(); j++) {  
             result.push_back(s + v[j]);
         }
     }
 }
 
-int chooseDistance(int t              // Максимальна сума відстаней
-    , int k            // Кількість міст для відвідування
-    , vector<int> ls     // Список відстаней, всі відстані є додатними або нульовими цілими числами
+int chooseDistance(int t        // Максимальна сума відстаней
+    , int k                     // Кількість міст для відвідування
+    , vector<int> ls            // Список відстаней, всі відстані є додатними або нульовими цілими числами
 ) {
 
-    if (ls.size() < k) { // 2 < 1 -
-        //cout << -1 << " first " << k <<endl;
-        //print("first");
-        //print(k);
+    if (ls.size() < k) { 
         return -1;
     }
     vector<int> result;
-    //sum(s, vv1, 1, k, k, result1, -1);
     sum(0, ls, 1, k, k, result, -1);
 
     sort(result.begin(), result.end());
-    
-    //int lastSeeEl = result[0];
+   
     int lastSeeEl = 0;
     for (auto element : result) {
         if (element < t) {
